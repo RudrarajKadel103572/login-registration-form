0b38096498832442c5f54355370171417097687e
RudrarajKadel103572 <71115155+RudrarajKadel103572@users.noreply.github.com>
2020-09-11 10:10:32 +0530

    Create config.php

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+//Enter DBSERVER,USERNAME, PASSWORD,NAME
+define('DB_SERVER', '');
+define('DB_USERNAME', '');
+define('DB_PASSWORD', '');
+define('DB_NAME', '');
+
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+?>
